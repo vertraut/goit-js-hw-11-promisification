e2c140bfaa3e34ebf2c91cfed445fc728457227c
vertraut <onlinemovement@gmail.com>
2020-11-05 22:54:52 +0200

    Исправление предачи аргумента в resolve

--- js/3.js
+++ js/3.js
@@ -3,13 +3,14 @@ const randomIntegerFromInterval = (min, max) => {
 };
 
 const makeTransaction = (transaction) => {
-  const delay = randomIntegerFromInterval(200, 500);
   return new Promise((resolve, reject) => {
+    const delay = randomIntegerFromInterval(200, 500);
+
     setTimeout(() => {
       const canProcess = Math.random() > 0.3;
 
       if (canProcess) {
-        resolve(transaction.id, delay);
+        resolve({ id: transaction.id, time: delay });
       } else {
         reject(transaction.id);
       }
@@ -17,7 +18,7 @@ const makeTransaction = (transaction) => {
   });
 };
 
-const logSuccess = (id, time) => {
+const logSuccess = ({ id, time }) => {
   console.log(`Transaction ${id} processed in ${time}ms`);
 };
 
